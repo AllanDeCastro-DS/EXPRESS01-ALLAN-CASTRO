d8fca4b40d7637f56b13a23268783bbd3158c650
Allan de Castro <116167880+AllanDeCastro-DS@users.noreply.github.com>
2024-08-02 11:40:06 -0300

    sistema de rotas entre paginas via botoes

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ server.get('/json', (req, res) => {
 })
 
 server.use((req,res)=>{
-    //res.send('Erro!!!');
-    //para mostrar o status code correto
     res.status(404).send("<h1>Erro!!</h1>");
 })
 server.listen(3000);
--- server2.js
+++ server2.js
@@ -7,7 +7,7 @@ server.get('/',(req,res)=>{
     res.sendFile('./htmls/principal.html', {root:__dirname})
 })
 
-server.get('/servicos',(req,res)=>{
+server.get('/Servicos',(req,res)=>{
     res.sendFile('./htmls/servicos.html', {root:__dirname})
 })
 
@@ -15,8 +15,14 @@ server.get('/Sobre',(req,res)=>{
     res.sendFile('./htmls/sobre.html', {root:__dirname})
 })
 
+server.get('/Lista',(req,res)=>{
+    res.sendFile('./htmls/lista.html', {root:__dirname})
+})
+
 server.use((req,res)=>{
     res.status(404).sendFile('./htmls/404.html', {root:__dirname})
 })
+
+
 
 server.listen(3000);
